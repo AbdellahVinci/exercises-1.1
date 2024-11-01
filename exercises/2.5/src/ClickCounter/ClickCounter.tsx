e3a4b283da0f754d656ec5b012b8aa03448e9ac5
Abdellah Azzouz <abdellah.azzouz@student.vinci.be>
2024-11-01 01:50:11 +0100

    new:ex2.5-events

--- exercises/2.5/src/ClickCounter/ClickCounter.tsx
+++ exercises/2.5/src/ClickCounter/ClickCounter.tsx
@@ -9,15 +9,23 @@ type ClickCounterProps={
 
 const ClickCounter: React.FC< ClickCounterProps> = ({titre,message}) => {
   const [count, setCount] = useState(0);
+  const [isHovered, setIsHovered] = useState(false);
+
 
   return (
     <div>
     <h2>{titre}</h2>
-      <button onClick={() => setCount(count + 1)}>
-        Count is {count}
-      </button>
-      {count >=10 && <p>{message}</p>}
-    </div>
+    {isHovered && <p>Please click on me now!</p>}
+    <button
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      onClick={() => setCount(count + 1)}
+    >
+      Count is {count}
+    </button>
+    {count >= 10 && <p>{message}</p>}
+  </div>
+  
   );
 };
 
