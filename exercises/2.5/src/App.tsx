fc984b64fe8a512fa44e9c67440b00d04238afa9
Abdellah Azzouz <abdellah.azzouz@student.vinci.be>
2024-11-01 01:47:09 +0100

    new:ex2.5-destructuring

--- exercises/2.5/src/App.tsx
+++ exercises/2.5/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <ClickCounter />
+      <ClickCounter titre="Click Counter" message="You are a master in the art of clicking!" />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
--- exercises/2.5/src/ClickCounter/ClickCounter.tsx
+++ exercises/2.5/src/ClickCounter/ClickCounter.tsx
@@ -1,14 +1,22 @@
 
 import { useState } from 'react';
 
-const ClickCounter = () => {
+type ClickCounterProps={
+    titre:string;
+    message:string;
+
+}
+
+const ClickCounter: React.FC< ClickCounterProps> = ({titre,message}) => {
   const [count, setCount] = useState(0);
 
   return (
     <div>
+    <h2>{titre}</h2>
       <button onClick={() => setCount(count + 1)}>
         Count is {count}
       </button>
+      {count >=10 && <p>{message}</p>}
     </div>
   );
 };
