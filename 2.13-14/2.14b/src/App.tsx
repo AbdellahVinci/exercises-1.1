5082621a2f71f472189d8616b08994feb8facf26
Abdellah Azzouz <abdellah.azzouz@student.vinci.be>
2024-11-25 15:20:43 +0100

    new:ex2.14b-refactor

---
+++ 2.13-14/2.14b/src/App.tsx
@@ -0,0 +1,22 @@
+import {  useState } from "react";
+import RandomDog from "./RandomDog";
+
+
+
+const App = () => {
+  const [refresh] = useState(false);
+
+  return (
+    <>
+      <div style={{ display: "flex", flexDirection: "row", gap: 10 }}>
+        <RandomDog key={`${refresh}1`} />
+        <RandomDog key={`${refresh}2`} />
+        <RandomDog key={`${refresh}3`} />
+      </div>
+
+     
+    </>
+  );
+};
+
+export default App;
---
+++ 2.13-14/2.14b/src/RandomDog.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+
+interface Dog {
+    message: string;
+    status: string;
+  }
+
+  const RandomDog = () => {
+    const [dog, setDog] = useState<Dog | undefined>(undefined);
+
+    
+
+  const fetchDogImage = async () => {
+    
+    try {
+
+      const response = await fetch("https://dog.ceo/api/breeds/image/random");
+
+
+      if (!response.ok)
+
+        throw new Error(
+
+          `fetch error : ${response.status} : ${response.statusText}`
+
+        );
+
+        const dog = await response.json();
+
+        setDog(dog);
+  
+      } catch (err) {
+  
+        console.error("HomePage::error: ", err);
+  
+      }
+  
+    };
+
+    useEffect(() => {
+      fetchDogImage();
+    }, []);
+
+  if (!dog) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <h3>Random dog</h3>
+      <img src={dog.message} alt="Random dog" style={{ maxHeight: 300 }} />
+    </div>
+  );
+};
+
+
+export default RandomDog;
